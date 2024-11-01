b20ab967cca9b530a37ae58e979bc0da82d83e10
tommykw <destinys.lotus.1108mn@gmail.com>
2024-11-01 12:02:46 +0900

    chore: update biome.json configuration

--- src/withSeparators/index.tsx
+++ src/withSeparators/index.tsx
@@ -1,20 +1,20 @@
 import React, { type ReactNode } from 'react';
 
 export const withSeparators = (items: ReactNode[]) => ({
-	separatedBy(separator: ReactNode): ReactNode[] {
-		return items.map((item, index) => (
-			<React.Fragment
-				key={`react-list-separator-item-${/* biome-ignore lint/suspicious/noArrayIndexKey: Temporary ignore - needs proper key implementation instead of array index */ index}`}
-			>
-				{item}
-				{index < items.length - 1 && (
-					<React.Fragment
-						key={`react-list-separator-sep-${/* biome-ignore lint/suspicious/noArrayIndexKey: Temporary ignore - needs proper key implementation instead of array index */ index}`}
-					>
-						{separator}
-					</React.Fragment>
-				)}
-			</React.Fragment>
-		));
-	},
+  separatedBy(separator: ReactNode): ReactNode[] {
+    return items.map((item, index) => (
+      <React.Fragment
+        key={`react-list-separator-item-${/* biome-ignore lint/suspicious/noArrayIndexKey: Temporary ignore - needs proper key implementation instead of array index */ index}`}
+      >
+        {item}
+        {index < items.length - 1 && (
+          <React.Fragment
+            key={`react-list-separator-sep-${/* biome-ignore lint/suspicious/noArrayIndexKey: Temporary ignore - needs proper key implementation instead of array index */ index}`}
+          >
+            {separator}
+          </React.Fragment>
+        )}
+      </React.Fragment>
+    ));
+  },
 });
